f74517443cd6c0d21fe0ee8969c701f09884c683
Sam Barrie <sam.dbarrie@gmail.com>
2025-06-23 21:18:07 +0100

    refactor: remove Card component to streamline structure and improve code clarity

--- src/components/Card.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react'
-import { CARD } from '../classNames'
-
-export function Card({ children }: { children: ReactNode }) {
-  return <div className={CARD}>{children}</div>
-}
